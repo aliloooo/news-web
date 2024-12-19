9e8c8984c4d887c0110ba2ff9d5d6e3ceba910cc
aliloooo <aidp1112@gmail.com>
2024-12-19 22:25:09 +0700

    done project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // Import Navigate
 import Navbar from './components/Navbar';
 import Search from './components/Search';
 import Indonesia from './pages/Indonesia';
@@ -14,6 +14,7 @@ const App = () => (
       <Route path="/programming" element={<Programming />} />
       <Route path="/saved" element={<Saved />} />
       <Route path="/search" element={<Search />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </Router>
 );
