a16c815e27de1c819f0a76ec4d67f0ea1eaed84d
aidp <alridp@aidp-2.local>
2026-01-19 14:13:18 +0700

    Update Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import Layout from './components/Layout';
 import Indonesia from './pages/Indonesia';
 import Programming from './pages/Programming';
